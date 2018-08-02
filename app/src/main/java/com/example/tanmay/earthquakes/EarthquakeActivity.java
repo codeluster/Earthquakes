bd60a6938a6d1c8a7cdce77994c5591c1093795d
tanma <tanmaysingal@icloud.com>
2018-08-02 11:20:45 +0530

    Colorful circles behind magnitude text and removed dividers in list view

--- app/src/main/java/com/example/tanmay/earthquakes/EarthquakeActivity.java
+++ app/src/main/java/com/example/tanmay/earthquakes/EarthquakeActivity.java
@@ -1,8 +1,10 @@
 package com.example.tanmay.earthquakes;
 
 import android.content.Context;
+import android.graphics.drawable.GradientDrawable;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -64,6 +66,12 @@ public class EarthquakeActivity extends AppCompatActivity {
 
             float magnitude_raw = currentEarthquake.getMagnitude();
 
+            GradientDrawable magnitudeCircle = (GradientDrawable) magnitudeView.getBackground();
+
+            int magnitudeColor = getMagnitudeColor(magnitude_raw);
+
+            magnitudeCircle.setColor(magnitudeColor);
+
             // Convert all magnitudes into decimal with one place
             DecimalFormat formatter = new DecimalFormat("0.0");
             String magnitude_formatted = formatter.format(magnitude_raw);
@@ -106,6 +114,51 @@ public class EarthquakeActivity extends AppCompatActivity {
         private String formatTime(Date date) {
             SimpleDateFormat timeFormat = new SimpleDateFormat("h:mm a");
             return timeFormat.format(date);
+        }
+
+        private int getMagnitudeColor(float magnitude) {
+
+            int magnitudeColorResourceId = 0;
+
+            switch ((int) Math.floor(magnitude)) {
+
+                case 1:
+                    magnitudeColorResourceId = R.color.magnitude1;
+                    break;
+                case 2:
+                    magnitudeColorResourceId = R.color.magnitude2;
+                    break;
+                case 3:
+                    magnitudeColorResourceId = R.color.magnitude3;
+                    break;
+                case 4:
+                    magnitudeColorResourceId = R.color.magnitude4;
+                    break;
+                case 5:
+                    magnitudeColorResourceId = R.color.magnitude5;
+                    break;
+                case 6:
+                    magnitudeColorResourceId = R.color.magnitude6;
+                    break;
+                case 7:
+                    magnitudeColorResourceId = R.color.magnitude7;
+                    break;
+                case 8:
+                    magnitudeColorResourceId = R.color.magnitude8;
+                    break;
+                case 9:
+                    magnitudeColorResourceId = R.color.magnitude9;
+                    break;
+                case 10:
+                    magnitudeColorResourceId = R.color.magnitude10plus;
+                    break;
+                default:
+                    magnitudeColorResourceId = android.R.color.black;
+                    break;
+            }
+
+            return ContextCompat.getColor(getContext(), magnitudeColorResourceId);
+
         }
 
     }
